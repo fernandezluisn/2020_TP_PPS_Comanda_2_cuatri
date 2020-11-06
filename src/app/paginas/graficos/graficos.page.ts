7ec2ff71408fdfb887e675d3f9f8d36d27768efb
Luis Fernandez <fernandezluisn@gmail.com>
2020-11-06 19:11:38 -0400

    encuestas a empleados

--- src/app/paginas/graficos/graficos.page.ts
+++ src/app/paginas/graficos/graficos.page.ts
@@ -23,6 +23,14 @@ export class GraficosPage implements OnInit {
   falsosFalta=0;
   verdaderosRoturas=0;
   falsosRoturas=0;
+  muchas=0;
+  normal=0;
+  poca=0;
+  mp=0;
+
+  ordenado=0;
+  desorden=0;
+  mDesorden=0;
   constructor(private service:EncuestasService) { 
     this.service.GetEncuestasEmpleados().subscribe(lista=>{
       this.encuestasEmpleados=lista;
@@ -45,6 +53,24 @@ export class GraficosPage implements OnInit {
         this.verdaderosFalta++;    
       }else{
         this.falsosFalta++;
+      }
+
+      if(elem.demora=="Mucha"){
+        this.muchas++;
+      }else if(elem.demora=="Normal"){
+        this.normal++;
+      }else if(elem.demora=="Poca"){
+        this.poca++;
+      }else{
+        this.mp++;
+      }
+
+      if(elem.orden=="Ordenado"){
+        this.ordenado++;
+      }else if(elem.orden=="Desordenado"){
+        this.desorden++;
+      }else{
+        this.mDesorden++;
       }
     });   
   }
@@ -70,9 +96,18 @@ export class GraficosPage implements OnInit {
 
   public barChartData: ChartDataSets[] = [
     { data: [this.verdaderosFalta, this.verdaderosFila, this.verdaderosRoturas], label: 'Si' },
-    { data: [this.falsosFalta, this.falsosFila, this.falsosRoturas], label: 'No' }
+    { data: [this.verdaderosFalta, this.verdaderosFila, this.verdaderosRoturas], label: 'No' }
   ];
 
-  //falta limpieza, demora y orden
+  //falta limpieza y orden
 
+  public dataDemora: ChartDataSets[] = [
+    { data: [this.muchas, this.normal, this.poca, this.mp], label: '' }    
+  ];
+  public demoraLabels: Label[] = ['Mucha', 'Normal', 'Poca', "Muy poca"];
+
+  public dataOrden: ChartDataSets[] = [
+    { data: [this.ordenado, this.desorden, this.mDesorden], label: '' }    
+  ];
+  public ordenLabels: Label[] = ["Ordenado", "Desordenado", "Muy desordenado"];
 }
