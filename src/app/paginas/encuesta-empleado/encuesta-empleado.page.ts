53ce6d4e8c4e76c1cb1a311e720f87b35195e6c9
Luis Fernandez <fernandezluisn@gmail.com>
2020-11-18 09:50:09 -0400

    encuesta empleado

--- src/app/paginas/encuesta-empleado/encuesta-empleado.page.ts
+++ src/app/paginas/encuesta-empleado/encuesta-empleado.page.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { Router } from '@angular/router';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
-import { Vibration } from '@ionic-native/vibration/ngx';
-import { AlertController, LoadingController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { finalize } from 'rxjs/operators';
 import { EncuestaEmpleado } from 'src/app/interfaces/encuestaEmpleado';
 import { AuthService } from 'src/app/servicios/auth.service';
 import { EncuestasService } from 'src/app/servicios/encuestas.service';
+import { SpinnerService } from 'src/app/servicios/spinner.service';
 import { ToastService } from 'src/app/servicios/toast.service';
 
 
@@ -17,7 +18,7 @@ import { ToastService } from 'src/app/servicios/toast.service';
 })
 export class EncuestaEmpleadoPage implements OnInit {
 
-  loading;
+  
   image: string = null;
   url1: string;
   opcion:string;
@@ -33,8 +34,8 @@ export class EncuestaEmpleadoPage implements OnInit {
   constructor(  private encuestasService: EncuestasService,  private camera: Camera, private storage:AngularFireStorage,
     private router:Router,
     private alertController:AlertController, 
-    private loadingCtrl:LoadingController,
-    private vibra:Vibration,
+    private spinner:SpinnerService,
+    
     private toast:ToastService,private auth:AuthService
 ) { 
   this.opcion="Ordenado";
@@ -45,16 +46,7 @@ export class EncuestaEmpleadoPage implements OnInit {
   ngOnInit() {
   }
 
-  async presentLoading(message: string) {
-    this.loading = await this.loadingCtrl.create({
-        message,
-        spinner: "crescent",
-        duration: 2000
-    });
-    return this.loading.present();
-
-    
-  }
+  
 
   async alertar(mensaje:string){
     const alert= this.alertController.create({
@@ -68,7 +60,7 @@ export class EncuestaEmpleadoPage implements OnInit {
   }
 
   async sacarFoto(){
-    this.presentLoading("Cargando imagen");
+    this.spinner.showSpinner();
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
@@ -79,10 +71,34 @@ export class EncuestaEmpleadoPage implements OnInit {
 
     await this.camera.getPicture(options).then((imageData) => {
       this.image = `data:image/jpeg;base64,${imageData}`;   
+
+      try{        
+        const com=this.usuario.id+Date.now().toString();
+        let img;
+        fetch(this.image)
+        .then(res => res.blob().then(r=>{
+          img=r
+        }))
+        
+        const file= img;
+        const path= com;
+        const ref=this.storage.ref(path);    
+        const task=this.storage.upload(path, file);     
+        task.snapshotChanges().pipe(finalize(()=>ref.getDownloadURL().subscribe(url=>{
+          this.url1=url;       
+        
+        } ))).subscribe(); 
+        }catch(e){
+          this.alertar(e);
+        }
         
      }, (err) => {
       this.alertar(err);
      });
+
+     
+
+     this.spinner.hideSpinner();
   }
 
   subir(){
